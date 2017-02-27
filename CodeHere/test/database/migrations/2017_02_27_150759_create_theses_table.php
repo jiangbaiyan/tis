f7f548645a45bee6090f05a894a46e37d6c534f1
yangbingyan <yangbingyan159@163.com>
2017-02-27 23:21:59 +0800

    添加account和thesis的数据库迁移及模型类

---
+++ CodeHere/test/database/migrations/2017_02_27_150759_create_theses_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThesesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('theses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("name")->nullable();
+            $table->string("thesis_topic")->nullable();
+            $table->string("periodical_or_conference")->nullable();
+            $table->string("ISSN_or_ISBN")->nullable();
+            $table->string("issue")->nullable();
+            $table->string("volume")->nullable();
+            $table->string("page_number")->nullable();
+            $table->dateTime("publication_year")->nullable();
+            $table->dateTime("publication_time")->nullable();
+            $table->string("SCI")->nullable();
+            $table->string("EI")->nullable();
+            $table->string("CCF")->nullable();
+            $table->string("is_include_by_domestic_periodical")->nullable();
+            $table->string("accession_number")->nullable();
+            $table->text("remark")->nullable();
+            $table->string("author_rank")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('theses');
+    }
+}
