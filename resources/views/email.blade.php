66f2cb3cecaeb610884585701f97b3b99fb6ad9e
jiangbaiyan_i <jiangbaiyan_i>
2018-08-29 17:34:51 +0800

    发送邮件模板

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -3,19 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <style>
-        p{
-            color: red;
-            font-weight: bold;
+        #download{
+            color: #ff3333;
+            font-weight: 400;
         }
     </style>
 </head>
 <body>
 <h2>{{$name}}，您好</h2>
-<p>此邮件由通知系统自动发出</p>
-<p>点击查看或下载附件: </p>
-<p>注：如遇到不能直接点击链接打开的情况，请复制该链接到浏览器地址栏访问即可</p>
+<p id="download">点击查看或下载附件: </p>
 @foreach($fileUrls as $fileUrl)
     <p>{{$fileUrl}}</p>
 @endforeach
+<br>
+<p>此邮件由杭电网安信息平台通知系统自动发出</p>
+<p>注：如遇到不能直接点击链接打开的情况，请复制该链接到浏览器地址栏访问即可</p>
 </body>
 </html>
