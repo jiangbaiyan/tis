fc3447a4e40457bf4b8dce74ba2ee76978165b77
jiangbaiyan_i <jiangbaiyan_i>
2018-09-04 20:26:32 +0800

    css优化

--- resources/views/showauthstate.blade.php
+++ resources/views/showauthstate.blade.php
@@ -22,32 +22,34 @@
         姓名：{{$item->name}}
         通知模块权限：
         @if ($item->info_auth_state == \App\Http\Model\Teacher::NORMAL)
-            普通教师：<input type="radio" name="info_auth_state" value="0" checked>
-            辅导员：<input type="radio" name="info_auth_state" value="1">
-            教务老师：<input type="radio" name="info_auth_state" value="2">
+            普通教师<input type="radio" name="info_auth_state" value="0" checked>
+            辅导员<input type="radio" name="info_auth_state" value="1">
+            教务老师<input type="radio" name="info_auth_state" value="2">
         @elseif ($item->info_auth_state == \App\Http\Model\Teacher::INSTRUCTOR)
-            普通教师：<input type="radio" name="info_auth_state" value="0">
-            辅导员：<input type="radio" name="info_auth_state" value="1" checked>
-            教务老师：<input type="radio" name="info_auth_state" value="2">
+            普通教师<input type="radio" name="info_auth_state" value="0">
+            辅导员<input type="radio" name="info_auth_state" value="1" checked>
+            教务老师<input type="radio" name="info_auth_state" value="2">
         @else
-            普通教师：<input type="radio" name="info_auth_state" value="0">
-            辅导员：<input type="radio" name="info_auth_state" value="1">
-            教务老师：<input type="radio" name="info_auth_state" value="2" checked>
+            普通教师<input type="radio" name="info_auth_state" value="0">
+            辅导员<input type="radio" name="info_auth_state" value="1">
+            教务老师<input type="radio" name="info_auth_state" value="2" checked>
         @endif
+        |
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
         请假模块权限：
         @if ($item->leave_auth_state == \App\Http\Model\Teacher::NORMAL)
-            普通教师：<input type="radio" name="leave_auth_state" value="0" checked>
-            辅导员：<input type="radio" name="leave_auth_state" value="1">
-            教务老师：<input type="radio" name="leave_auth_state" value="2">
+            普通教师<input type="radio" name="leave_auth_state" value="0" checked>
+            辅导员<input type="radio" name="leave_auth_state" value="1">
+            教务老师<input type="radio" name="leave_auth_state" value="2">
         @elseif ($item->leave_auth_state == \App\Http\Model\Teacher::INSTRUCTOR)
-            普通教师：<input type="radio" name="leave_auth_state" value="0">
-            辅导员：<input type="radio" name="leave_auth_state" value="1" checked>
-            教务老师：<input type="radio" name="leave_auth_state" value="2">
+            普通教师<input type="radio" name="leave_auth_state" value="0">
+            辅导员<input type="radio" name="leave_auth_state" value="1" checked>
+            教务老师<input type="radio" name="leave_auth_state" value="2">
         @else
-            普通教师：<input type="radio" name="leave_auth_state" value="0">
-            辅导员：<input type="radio" name="leave_auth_state" value="1">
-            教务老师：<input type="radio" name="leave_auth_state" value="2" checked>
+            普通教师<input type="radio" name="leave_auth_state" value="0">
+            辅导员<input type="radio" name="leave_auth_state" value="1">
+            教务老师<input type="radio" name="leave_auth_state" value="2" checked>
         @endif
             <input type="hidden" name="uid" value="{{$item->uid}}">
             <input type="submit" value="点击修改权限">
