6df8d2a502909b8158b21dbbf6ce9f877ac3d150
jiangbaiyan_i <jiangbaiyan_i>
2018-09-06 19:46:38 +0800

    表结构修改

--- database/migrations/2018_09_06_150229_create_teach_reach_state_table.php
+++ database/migrations/2018_09_06_150229_create_teach_reach_state_table.php
@@ -18,7 +18,7 @@ class CreateTeachReachStateTable extends Migration
             $table->string('cg',1024)->default('')->comment('课程目标达成度');
             $table->string('gg',1024)->default('')->comment('毕业要求指标点达成度');
             $table->string('course_name')->default('')->comment('课程名称');
-            $table->unsignedSmallInteger('year')->default('0')->comment('学年');
+            $table->string('year',32)->default('')->comment('学年');
             $table->unsignedTinyInteger('term')->default('0')->commet('学期');
             $table->integer('teacher_id')->default('0')->comment('教师id');
             $table->timestamps();
