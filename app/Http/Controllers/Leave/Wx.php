837afcc3f5c33185e4628d24b750890a24391a7f
jiangbaiyan_i <jiangbaiyan_i>
2018-09-09 23:19:39 +0800

    修复bug

--- app/Http/Controllers/Leave/Wx.php
+++ app/Http/Controllers/Leave/Wx.php
@@ -46,7 +46,7 @@ class Wx{
         }
         if (strtotime($params['begin_time']) >= strtotime($params['end_time'])){
             Logger::notice('leave|illegal_leave_time|params:' . json_encode($params));
-            throw new ParamValidateFailedException('请假起止时间不合法，请重新输入');
+            throw new OperateFailedException('请假起止时间不合法，请重新输入');
         }
         $user = User::getUser();
         $data['leave_reason'] = $params['leave_reason'];
